2a8141710285cfb505a0395d9c2572ea1eaefb2c
liuxiong <liuxiong332@163.com>
2015-11-26 10:22:57 +0800

    Replace the new react-css-module

--- react-css-module/webpack.config.js
+++ react-css-module/webpack.config.js
@@ -1,36 +1,30 @@
-/**
- * The examples provided by Formidable Labs are for non-commercial testing and
- * evaluation purposes only. Formidable Labs reserves all rights not expressly
- * granted.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
- * FORMIDABLE LABS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
- * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
-
 var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  cache: true,
-  entry: {
-    app: "./examples/client.js"
-  },
+  entry: ['./app.js'],
   output: {
-    path: path.join(__dirname),
-    publicPath: '/',
-    filename: '[name].js',
-    chunkFilename: '[chunkhash].js'
+    path: path.join(__dirname, 'dist/'),
+    filename: 'bundle.js',
+    publicPath: '/assets/'
   },
+
+  resolve: {
+    extensions: ['', '.webpack.js', '.web.js', '.js', '.css', '.scss'],
+  },
+
   module: {
     loaders: [
-      {
-        test: /\.jsx?$/,
-        loader: 'babel-loader'
-      }
+      { test: /\.js/, loader: 'babel?cacheDirectory', exclude: /node_modules/ },
+      { test: /\.scss$/, loaders: ['style-loader', 'css-loader?modules&localIdentName=[name]__[local]___[hash:base64:5]', 'sass'] },
     ]
-  }
-}
+  },
+
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('development')
+      }
+    })
+  ]
+};
